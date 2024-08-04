d1264ffad64c2e2ece0a6e2836030b0695334309
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-04 19:47:59 -0300

    Componente EliminarEquipo

--- client/src/components/EliminarEquipo.jsx
+++ client/src/components/EliminarEquipo.jsx
@@ -6,7 +6,21 @@ const EliminarEquipo = () => {
 
         <div>
 
-            <h2>Eliminar Equipo</h2>
+            <div>
+
+                <h2>Eliminar Equipo</h2>
+
+                <form id='buscar-equipo'>
+
+                    <label htmlFor='nombre'>Modelo:</label>
+
+                    <input type='text' id='nombre' name='nombre' />
+
+                    <button type='submit'>Eliminar</button>
+
+                </form>
+
+            </div>
 
         </div>
     )
