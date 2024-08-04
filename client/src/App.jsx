0c8be987adc35418fcd6037482f2e2618d51fb0e
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-04 17:34:31 -0300

    FrontEnd ---> React + Vite

---
+++ client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import "./styles/App.css"
+import Home from './components/Home'
+
+const App = () => {
+
+  return (
+
+    <>
+
+      <Home />
+
+    </>
+
+  )
+
+}
+
+export default App
---
+++ client/src/components/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Home = () => {
+
+    return (
+
+        <div>
+
+            <h1>Proyecto MERN -- FullStack</h1>
+
+        </div>
+
+    )
+
+}
+
+export default Home
