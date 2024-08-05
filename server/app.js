28bad74c2785d8c18a23b6b6941b1c17a5292b48
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-05 13:41:12 -0300

    Agregqado de todas las rutas del CRUD

--- server/app.js
+++ server/app.js
@@ -6,7 +6,12 @@ import logger from 'morgan';
 
 import indexRouter from './routes/index.js';
 import getRouter from './routes/getRig.js';
-
+import getRigByModelRouter from './routes/getRigByModel.js';
+import getRigByIdRouter from './routes/getRigById.js';
+import createRigRouter from './routes/createRig.js';
+import updateRigRouter from './routes/updateRig.js';
+import deleteRigRouter from './routes/deleteRig.js';
+import apiExternaRouter from './routes/apiExterna.js';
 
 
 const app = express();
@@ -19,5 +24,11 @@ app.use(logger('dev'));
 
 app.use('/api/', indexRouter);
 app.use('/api/getRig', getRouter);
+app.use('/api/getRigByModel', getRigByModelRouter);
+app.use('/api/getRigById', getRigByIdRouter);
+app.use('/api/createRig', createRigRouter);
+app.use('/api/updateRig', updateRigRouter);
+app.use('/api/deleteRig', deleteRigRouter);
+app.use('/apiexterna', apiExternaRouter);
 
 export default app;
