7c5ab38bd57296c39acad796423ba1026d1f2079
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-13 13:52:41 -0300

    Prettier

--- server/middlewares/validateModel.js
+++ server/middlewares/validateModel.js
@@ -1,27 +1,19 @@
-import Rig from '../models/rig.model.js'
+import Rig from "../models/rig.model.js";
 
 const validateModel = async (req, res, next) => {
+  try {
+    const searchModel = await Rig.findOne({
+      model: req.params.model.toUpperCase(),
+    });
 
-    try {
-
-        const searchModel = await Rig.findOne({ model: req.params.model.toUpperCase() })
-
-        if ( searchModel !== null ) {
-
-            next()
-
-        } else {
-
-            res.status(401).json({ msg: 'Model already exists' })
-
-        }
-
-    } catch (error) {
-
-        res.status(401).json(error)
-
+    if (searchModel !== null) {
+      next();
+    } else {
+      res.status(401).json({ msg: "Model already exists" });
     }
+  } catch (error) {
+    res.status(401).json(error);
+  }
+};
 
-}
-
-export default validateModel
+export default validateModel;
