3450eb09f9d03d5bce0684fc075042bb78ae626c
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-13 13:48:33 -0300

    Agregado de validacion de modelo

--- server/routes/getRigByModel.js
+++ server/routes/getRigByModel.js
@@ -1,10 +1,9 @@
-import express from 'express'
-import getRigsController from '../controllers/getRigsController.js'
-import validateModel from '../middlewares/validateModel.js'
+import express from "express";
+import getRigsController from "../controllers/getRigsController.js";
+import validateModel from "../middlewares/validateModelForDelete.js";
 
-const router = express.Router()
+const router = express.Router();
 
+router.get("/:model", validateModel, getRigsController.getRigByModel);
 
-router.get('/:model', validateModel, getRigsController.getRigByModel)
-
-export default router
+export default router;
