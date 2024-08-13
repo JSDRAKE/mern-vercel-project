11a0d70b34b4fd871730590eb31bc640582d7cd5
JSDRAKE <jsdrake@jsdrake.com.ar>
2024-08-13 13:52:32 -0300

    Borrado por modelo y no por ID

--- server/controllers/deleteRigContoller.js
+++ server/controllers/deleteRigContoller.js
@@ -1,23 +1,14 @@
-import Rig from '../models/rig.model.js'
+import Rig from "../models/rig.model.js";
 
 const deleteRigContoller = {
-
-    async deleteRig(req, res) {
-
-        try {
-
-            const rig = await Rig.findByIdAndDelete(req.params.id)
-
-            return res.status(200).json({ rig })
-
-        } catch (error) {
-
-            return res.status(400).json({ error })
-
-        }
-
+  async deleteRig(req, res) {
+    try {
+      const rig = await Rig.findOneAndDelete({ model: req.params.model });
+      return res.status(200).json({ rig });
+    } catch (error) {
+      return res.status(400).json({ error });
     }
+  },
+};
 
-}
-
-export default deleteRigContoller
+export default deleteRigContoller;
